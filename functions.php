a3a39ca653a93a81178cae64e3636f48dec9351a
jordnayjackson <33468610+jordnayjackson@users.noreply.github.com>
2021-06-21 18:34:21 -0400

    remove left sidebar
    https://wphow2s.com/center-twenty-fourteen-theme-and-remove-left-sidebar/

--- functions.php
+++ functions.php
@@ -194,3 +194,11 @@ function twentyfourteen_header_style() {}
 
 
 ?>
+
+<?php
+	// Removing left sidebar of Twenty Fourteen
+function remove_left_sidebar(){
+	unregister_sidebar( 'sidebar-1' );
+}
+add_action( 'widgets_init', 'remove_left_sidebar', 11 );
+?>
