025c01578a37a0c29e442a62b5e19f19abe0c78a
jordnayjackson <33468610+jordnayjackson@users.noreply.github.com>
2021-06-21 18:39:50 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -194,11 +194,3 @@ function twentyfourteen_header_style() {}
 
 
 ?>
-
-<?php
-	// Removing left sidebar of Twenty Fourteen
-function remove_left_sidebar(){
-	unregister_sidebar( 'sidebar-1' );
-}
-add_action( 'widgets_init', 'remove_left_sidebar', 11 );
-?>
